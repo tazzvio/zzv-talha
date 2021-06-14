03ca9364b8ece434e275ae16bf927eaa143c6be7
Talha <t.anis60.ta@gmail.com>
2021-06-14 21:44:25 +0500

    Updated mac

---
+++ osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp target/client/x86_64-darwin/Zzv-view src/osx/zzv.app/Contents/MacOS/
+cd src/osx
+mkdir -p private_keys
+echo "$PRIV_KEY" > private_keys/AuthKey_7QW2J46UP9.p8
+codesign -s "$APP_CERT_NAME" -f --entitlements entitlement.plist zzv.app
+xcrun productbuild --component zzv.app /Applications --sign "$INSTALLER_CERT_NAME" zzv.pkg
+xcrun altool --upload-app --file zzv.pkg --type osx --apiKey "$APPSTORE_KEY_ID" --apiIssuer "$APPSTORE_ISSUER_ID"
