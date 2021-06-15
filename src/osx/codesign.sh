3bab557ea5b99b31017afe4d0541d3050b9ea09f
Talha <t.anis60.ta@gmail.com>
2021-06-15 20:59:35 +0500

    Updated osx

--- src/osx/codesign.sh
+++ src/osx/codesign.sh
@@ -5,4 +5,4 @@ cd src/osx
 sed -e 's/CFBundleVersionPlaceHolder/'$GITHUB_RUN_NUMBER'/g' zzv.app/Contents/Info.plist > zzv.app/Contents/Temp.plist
 mv zzv.app/Contents/Temp.plist zzv.app/Contents/Info.plist
 cat zzv.app/Contents/Info.plist
-codesign -s "$APP_CERT_NAME" -f --entitlements entitlement.plist zzv.app
+# codesign -s "$APP_CERT_NAME" -f --entitlements entitlement.plist zzv.app
