2f2edf464b3542532f97bd56aa789d0c05e6343d
Talha <t.anis60.ta@gmail.com>
2021-06-15 20:56:59 +0500

    Updated osx

--- src/osx/codesign.sh
+++ src/osx/codesign.sh
@@ -4,4 +4,5 @@ cp target/client/x86_64-darwin/Zzv-view src/osx/zzv.app/Contents/MacOS/
 cd src/osx
 sed -e 's/CFBundleVersionPlaceHolder/'$GITHUB_RUN_NUMBER'/g' zzv.app/Contents/Info.plist > zzv.app/Contents/Temp.plist
 mv zzv.app/Contents/Temp.plist zzv.app/Contents/Info.plist
+cat zzv.app/Contents/Info.plist
 codesign -s "$APP_CERT_NAME" -f --entitlements entitlement.plist zzv.app
