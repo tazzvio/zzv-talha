95e60c0a09fd5bb4eb805d81b6494b9e55ab2614
Talha <t.anis60.ta@gmail.com>
2021-06-15 16:45:08 +0500

    Updated osx

--- osx.sh
+++ osx.sh
@@ -5,5 +5,5 @@ cp target/client/x86_64-darwin/Zzv-view src/osx/zzv.app/Contents/MacOS/
 cd src/osx
 # codesign -s "$APP_CERT_NAME" -f --entitlements entitlement.plist zzv.app
 sed -i 's/CFBundleVersionPlaceHolder/3/g' zzv.app/Contents/Info.plist
-xcrun productbuild --component zzv.app /Applications --sign "$INSTALLER_CERT_NAME" zzv.pkg
-xcrun altool --upload-app --file zzv.pkg --type osx --apiKey "$APPSTORE_KEY_ID" --apiIssuer "$APPSTORE_ISSUER_ID"
+# xcrun productbuild --component zzv.app /Applications --sign "$INSTALLER_CERT_NAME" zzv.pkg
+# xcrun altool --upload-app --file zzv.pkg --type osx --apiKey "$APPSTORE_KEY_ID" --apiIssuer "$APPSTORE_ISSUER_ID"
